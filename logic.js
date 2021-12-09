bddbb50e450d59fff3fb37bd69ec0b662abbba7c
rishitpurohit <94513622+rishitpurohit@users.noreply.github.com>
2021-12-09 21:38:32 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,12 @@
+var first_name = "Rishit" ;
+var last_name = "Purohit" ;
+console.log(first_name) ;
+console.log(last_name) ; 
+var lm = "50"
+console.log(lm) ;
+var dm = "5" 
+console.log(dm)
+var lc = "10"
+var ar = "8" ; 
+var lll = lc - ar 
+console.log(lll)
